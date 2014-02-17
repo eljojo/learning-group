dd0d315cd5b13c439ebccd53ff34fbb3bb8eb964
José Tomás Albornoz <jojo@eljojo.net>
2014-02-17 21:04:12 +0100

    Added changes made in hackday to sinatra app

--- examples/sinatra/server.rb
+++ examples/sinatra/server.rb
@@ -1,15 +1,33 @@
 require 'sinatra'
 set :bind, '0.0.0.0'
 
-guests = ['brigitte', 'magda', 'jose']
+guests = []
+presents = ["chips", "club mate", "hardcore drugs"]
 
 get '/' do
   @guests = guests
+  @presents = presents
   erb :index
 end
 
-get '/add/:name' do
-  @message = "hi #{params[:name]}, you're in the list!"
-  guests << params[:name]
+post '/add' do
+  guest_name = params[:guest_name].strip
+  gift       = params[:gift].strip
+
+  if guest_name.empty? then
+    @message = "Please put in your name"
+  elsif gift.empty? then
+    @message = "Hey #{guest_name}, you need to bring a gift!"
+  else
+    @message = "hi, #{guest_name}, you're in the list!"
+    presents.delete(gift)
+    person = {
+      :name => guest_name,
+      :gift => gift
+    }
+    guests << person
+  end
+
   erb :message
 end
+
