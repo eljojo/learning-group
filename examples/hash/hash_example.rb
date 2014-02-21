652109ad501619df50fa34cb737500743684eaff
José Tomás Albornoz <jojo@eljojo.net>
2014-02-21 11:12:11 -0300

    Added more examples for hashes

--- examples/hash/hash_example.rb
+++ examples/hash/hash_example.rb
@@ -3,6 +3,15 @@ first_guest = {
   gift: "club mate"
 }
 
+
+puts first_guest[:name] # => "jose"
+puts first_guest[:gift] # => "club mate"
+
+first_guest[:gift] = "coca cola"
+
+puts first_guest[:gift] # => "coca cola"
+
+
 second_guest = {
   name: "henry",
   gift: "coca-cola"
